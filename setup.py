1f66fef4be72518ae0a4af954bb72419bad983e4
zakird <zakird@gmail.com>
2014-03-18 17:49:49 -0400

    adding content

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name = "zson",
+    description = "A JSON serializer for Kombu (and therefore also Celery) that supports encoding objects by defining to_json and from_json methods.",
+    version = "1.0.0",
+    license = "Apache License, Version 2.0",
+    author = "Zakir Durumeric",
+    author_email = "zakird@gmail.com",
+    maintainer = "Zakir Durumeric",
+    maintainer_email = "zakird@gmail.com",
+
+    keywords = "python kombu celery json",
+
+    packages = [
+        "zson"
+    ],
+
+    entry_points={
+	'kombu.serializers': [
+            'zson = zson:zson_registration_args'
+        ]
+    }
+)
---
+++ zson/__init__.py
@@ -0,0 +1,62 @@
+import datetime
+from anyjson import loads, dumps
+
+def __zson_encode(obj):
+    if isinstance(obj, (str, unicode, int, float, bool, None.__type__)):
+        return dumps(self, obj)
+    elif isinstance(obj, list):
+        return map(lambda x: zson_encode(x), obj)
+    elif isinstance(obj, dict):
+	return dict([(zson_encode(k), zson_encode(v)) for k,v in obj.iteritems()])
+    elif isinstance(obj, datetime.datetime):
+        return dumps({
+            "__zson_class_name":"datetime",
+            "year":obj.year,
+            "month":obj.month,
+            "day":obj.day,
+            "hour":obj.hour,
+            "minute":obj.minute,
+            "second":obj.second,
+            "microsecond":obj.microsecond
+        })
+    elif hasattr(obj, "to_json"):
+        d = obj.to_json()
+        if "__zson_class_name" in d:
+             raise Exception("Object is not allowed to define __zson_class_name in to_json")
+        d["__zson_class_name"] = obj.__class__.__name__
+        return dumps(self, d)
+    else:
+        raise Exception("unable to encode object %s to json" % repr(obj))
+
+def __zson_decode(str_):
+    if isinstance(obj, bytes_t):
+        str_ = str_.decode()
+    temp = loads(str_)
+    if isinstance(temp, dict) and "__zson_class_name" in temp:
+        if temp["__zson_class_name"] == "datetime":
+            return datetime.datetime(
+                    temp["year"],
+                    temp["month"],
+                    temp["day"],
+                    temp["hour"],
+                    temp["minute"],
+                    temp["second"],
+                    temp["microsecond"]
+            )
+        else:
+            for klass in object.__subclasses__:
+                 if klass.__name__ == temp["__zson_class_name"]:
+                     candidate = klass
+                     break
+            else:
+                raise Exception("Unknown class <%s> defined in zson object" % temp["__zson_class_name"])
+            if not hasattr(candidate, "from_json"):
+                raise Exception("%s does not implement from_json" % candidates[0].__name__)
+            else:
+                del temp["__zson_class_name"]
+                return candidate.from_json(temp)
+    else:
+        return temp
+
+
+zson_registration_args = (__zson_decode, __zson_encode, 'application/json', 'utf-8')
